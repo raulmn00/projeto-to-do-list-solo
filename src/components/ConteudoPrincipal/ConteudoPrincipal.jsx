978ea784d584360cc89432eddac44f76e6166d2f
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-09-16 14:20:23 -0300

    feat: implement form create paleta

--- src/components/ConteudoPrincipal/ConteudoPrincipal.jsx
+++ src/components/ConteudoPrincipal/ConteudoPrincipal.jsx
@@ -1,5 +1,6 @@
 import './ConteudoPrincipal.css';
 import { PaletaLista } from '../PaletaLista/PaletaLista.jsx';
+import { FormCreate } from '../FormCreate/FormCreate';
 
 export function ConteudoPrincipal({
 	paletasState,
@@ -11,6 +12,7 @@ export function ConteudoPrincipal({
 		<>
 			<main className="conteudo-principal">
 				<div className="conteudo-principal-container">
+					<FormCreate setPaleta={setPaletaState} />
 					<PaletaLista
 						paletas={paletasState}
 						setPaletas={setPaletaState}
---
+++ src/components/FormCreate/FormCreate.jsx
@@ -0,0 +1,111 @@
+import './FormCreate.css';
+import { apiHelper } from '../utils/api/Api';
+import { useState } from 'react';
+
+export function FormCreate({ setPaleta }) {
+	const [paletaState, setPaletaCriada] = useState({});
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		await apiHelper.createPaleta(paletaState);
+		const paletas = await apiHelper.findAllPaletas();
+		setPaleta(paletas);
+	};
+	console.log(paletaState);
+	return (
+		<div className="form">
+			<form
+				onSubmit={handleSubmit}
+				className="form-inputs"
+				id="formPaleta"
+			>
+				<section>
+					<span>titulo:</span>
+					<input
+						type="text"
+						name="titulo"
+						onChange={(event) => {
+							setPaletaCriada({
+								...paletaState,
+								titulo: event.target.value,
+							});
+						}}
+					></input>
+				</section>
+				<section>
+					<span>descricao</span>
+					<input
+						type="text"
+						name="descricao"
+						onChange={(event) => {
+							setPaletaCriada({
+								...paletaState,
+								descricao: event.target.value,
+							});
+						}}
+					></input>
+				</section>
+				<section>
+					<span>foto:</span>
+					<input
+						type="text"
+						name="foto"
+						onChange={(event) => {
+							setPaletaCriada({
+								...paletaState,
+								foto: event.target.value,
+							});
+						}}
+					></input>
+				</section>
+				<section>
+					<span>preco:</span>
+					<input
+						type="number"
+						name="preco"
+						onChange={(event) => {
+							setPaletaCriada({
+								...paletaState,
+								preco: event.target.value,
+							});
+						}}
+					></input>
+					<span>sabor:</span>
+					<input
+						type="text"
+						name="sabor"
+						onChange={(event) => {
+							setPaletaCriada({
+								...paletaState,
+								sabor: event.target.value,
+							});
+						}}
+					></input>
+					<label htmlFor="possuiRecheio">Possui Recheio? </label>
+					<select
+						name="possuiRecheio"
+						id="possuiRecheio"
+						defaultValue={true}
+						onChange={(event) => {
+							setPaletaCriada({
+								...paletaState,
+								possuiRecheio: event.target.value,
+							});
+						}}
+					>
+						<option value={true}>Sim</option>
+						<option value={false}>Não</option>
+					</select>
+					{paletaState.possuiRecheio === 'true' ? (
+						<>
+							<label htmlFor="recheio">Sabor do Recheio: </label>
+							<input type="text" name="recheio" />
+						</>
+					) : null}
+				</section>
+				<button type="submit" className="btn-submit">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+}
